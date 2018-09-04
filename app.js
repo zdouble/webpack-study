51efe3d49c250fd4cc0866a1a0494648a0e5fa06
zdouble <872715485@qq.com>
2018-09-04 18:19:33 +0800

    配置babel-loader 打包react应用

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-function test () {
-  console.log('hello webpack');
-}
+import React from 'react';
+import { render } from 'react-dom';
 
-test();
+const App = () => <div>hello react</div>;
+
+// class App extends React.Component {
+//   render() {
+//     return <div>hello react</div>;
+//   }
+// }
+
+render(<App/>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
+const path = require('path');
 module.exports = {
   entry: './app.js',
   output: {
-    filename: './bundle.js'
-  }
-}
+    filename: './bundle.js',
+    path: path.resolve(__dirname, './dist/test'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        include: path.resolve(__dirname, './app.js'),
+      },
+    ],
+  },
+};
